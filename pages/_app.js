f47a2e3877b41f94eccaf66f382c2a8aa28db41b
Luis C <me@lucataco.com>
2023-02-13 00:52:57 -0500

    Change to Plausible

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,12 @@
-import { Analytics } from '@vercel/analytics/react';
+import Script from 'next/script'
 import { ChakraProvider } from '@chakra-ui/react'
 import './styles.css';
 
 function App({ Component, pageProps }) {
   return (
     <ChakraProvider>
+      <Script defer data-domain="readingmoods.vercel.app" src="https://data.lucata.co/js/script.js" />
       <Component {...pageProps} />
-      <Analytics />
     </ChakraProvider>
   )
 }
